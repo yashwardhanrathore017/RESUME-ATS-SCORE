a46fb894a340b5aef0ac73fe29f98b4b5fd572f7
Yashwardhan Rathore <123929513+yashwardhanrathore017@users.noreply.github.com>
2024-06-26 16:57:46 +0530

    Add files via upload

---
+++ ats.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import google.generativeai as genai
+import os
+import PyPDF2 as pdf
+
+
+genai.configure(api_key="your api key")
+
+
+#gemini function
+
+def get_gemini_response(input):
+    model=genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(input)
+    return response.text
+
+#convert pdf to text
+
+
+def input_pdf_text(uploaded_file):
+    text = ""
+    reader = pdf.PdfFileReader(uploaded_file)
+    for page_number in range(reader.numPages):
+        page = reader.getPage(page_number)
+        text += page.extractText()
+    return text
+
+
+input_prompt ="""
+
+### As a skilled Application Tracking System (ATS) with advanced knowledge in technology and data science, your role is to meticulously evaluate a candidate's resume based on the provided job description. 
+
+### Your evaluation will involve analyzing the resume for relevant skills, experiences, and qualifications that align with the job requirements. Look for key buzzwords and specific criteria outlined in the job description to determine the candidate's suitability for the position.
+
+### Provide a detailed assessment of how well the resume matches the job requirements, highlighting strengths, weaknesses, and any potential areas of concern. Offer constructive feedback on how the candidate can enhance their resume to better align with the job description and improve their chances of securing the position.
+
+### Your evaluation should be thorough, precise, and objective, ensuring that the most qualified candidates are accurately identified based on their resume content in relation to the job criteria.
+
+### Remember to utilize your expertise in technology and data science to conduct a comprehensive evaluation that optimizes the recruitment process for the hiring company. Your insights will play a crucial role in determining the candidate's compatibility with the job role.
+resume={text}
+jd={jd}
+### Evaluation Output:
+1. Calculate the percentage of match between the resume and the job description. Give a number and some explation
+2. Identify any key keywords that are missing from the resume in comparison to the job description.
+3. Offer specific and actionable tips to enhance the resume and improve its alignment with the job requirements.
+"""
+
+##stramlit
+
+st.title("DDS Smart ATS")
+st.text("Imporve your ATS resume score Match")
+jd = st.text_area("Paste job description here")
+uploaded_file= st.file_uploader("Upload your resume", type="pdf", help= "Please upload the pdf")
+
+submit =  st.button('Check Your Score')
+if submit:
+    if uploaded_file is not None:
+        text =  input_pdf_text(uploaded_file)
+        response=get_gemini_response(input_prompt.format(text=text, jd=jd))
+        st.subheader(response)
